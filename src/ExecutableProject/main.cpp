bbe50fb81a3811ce6e936b80ef20001d8bc40113
Andrés García Navarro <andrga37@ucm.es>
2026-02-17 09:51:35 +0100

    sdl libs

--- src/ExecutableProject/main.cpp
+++ src/ExecutableProject/main.cpp
@@ -6,7 +6,6 @@
 
 #define SOL_ALL_SAFETIES_ON 1
 #include <sol/sol.hpp>
-#include <SDL3/SDL.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,17 +21,6 @@ int main(int argc, char* argv[]) {
 	//bool isfullscreen = lua["config"]["fullscreen"]; // can get nested variables
 	//sol::table config = lua["config"];
 	//assert(!isfullscreen);
-
-    /*SDL_Window* window = SDL_CreateWindow("Example: 0", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, "");
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 250);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(2000);
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    SDL_Quit();
-    return EXIT_SUCCESS;*/
 
 	return 0;
 }
--- src/PlatformModule/PlatformModule.cpp
+++ src/PlatformModule/PlatformModule.cpp
@@ -4,7 +4,22 @@
 #include "pch.h"
 #include "framework.h"
 
+#include <SDL3/SDL.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
 // TODO: This is an example of a library function
 void fnPlatformModule()
 {
+    SDL_Window* window = SDL_CreateWindow("Example: 0", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+   SDL_Renderer* renderer = SDL_CreateRenderer(window, "");
+   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 250);
+   SDL_RenderClear(renderer);
+   SDL_RenderPresent(renderer);
+   SDL_Delay(2000);
+   SDL_DestroyWindow(window);
+   SDL_DestroyRenderer(renderer);
+   SDL_Quit();
+   return EXIT_SUCCESS;
 }
