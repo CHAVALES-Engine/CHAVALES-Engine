a3f639d5f0b4edd56a918fc6812a05f74f889f0e
Andrés García Navarro <andrga37@ucm.es>
2026-02-19 00:39:39 +0100

    dllLoader first version
    todo: hot-reloading, connection with ComponentsRegister

---
+++ projects/ExecutableProject/DLLLoader.cpp
@@ -0,0 +1,42 @@
+#include "DLLLoader.h"
+#include <Windows.h>
+
+DLLLoader::DLLLoader()
+{
+}
+
+DLLLoader::~DLLLoader()
+{
+	unLoadAll();
+}
+
+bool DLLLoader::load(const std::string& path)
+{
+	HMODULE library = nullptr;// Base direction of module in memory
+
+	// Windows search a library and loads on program memory
+	if ((library = LoadLibraryA(path.c_str())) == nullptr) return false;
+
+	// TODO: register components
+
+	// Adds library handler to vector
+	_libraries.push_back({ library, path });
+
+	return true;
+}
+
+void DLLLoader::unLoadAll()
+{
+	for (auto& library : _libraries) {
+		// Windows unloads library
+		FreeLibrary(library.handle);
+	}
+
+	_libraries.clear();
+}
+
+bool DLLLoader::unload(const std::string& path)
+{
+	// TODO
+	return true;
+}
---
+++ src/ExecutableProject/DLLLoader.h
@@ -0,0 +1,61 @@
+#pragma once
+/*
+*
+* @file DLLLoader
+* @brief DLL loader, this DLLLoader is only usable on windows.
+*/
+
+#include <string>
+#include <vector>
+
+class DLLLoader
+{
+public:
+	/**
+	* @brief Empty constructor.
+	*
+	*		
+	*/
+	DLLLoader();
+	/**
+	* @brief Destructor.
+	*
+	*		Unload all dynamic libraries.
+	*/
+	~DLLLoader();
+	/**
+	* @brief Load library method.
+	*
+	*		It loads the library on the path.
+	* @param path - Path to loaded dll.
+	* @return bool - It loaded correctly.
+	*/
+	bool load(const std::string& path);
+	/**
+	* @brief unload all libaries
+	*
+	*			Unloads all dynamic libraries, It will be replaced by other more hot-reload freandly.
+	*/
+	void unLoadAll();
+	/**
+	* @brief Unload library method.
+	*		
+	*		TODO: It unloads the library on the path.
+	* @param path - Path to unloaded dll.
+	* @return bool - It unloaded correctly.
+	*/
+	bool unload(const std::string& path);
+private:
+	struct LoadedLibrary {
+		HMODULE handle;
+		std::string path;
+	};
+	/**
+	* @brief
+	*
+	*		Vector that contains the information of a dll.
+	*/
+	std::vector<LoadedLibrary> _libraries;
+
+};
+
