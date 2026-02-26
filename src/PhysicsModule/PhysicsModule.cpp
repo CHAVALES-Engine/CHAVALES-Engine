66c2d7352d6aa37285981575a1d6d392ea1d1324
julepage <julepage@ucm.es>
2026-02-26 20:28:43 +0100

    biblioteca physx 5.6.1
    Co-Authored-By: Sergio Naranjo <157891236+DiverBro@users.noreply.github.com>

--- src/PhysicsModule/PhysicsModule.cpp
+++ src/PhysicsModule/PhysicsModule.cpp
@@ -1,6 +1,7 @@
 // PhysicModule.cpp : Defines the functions for the static library.
 //
-
+#include "../../dependencies/PhysX/physx/include/PxPhysicsAPI.h"
+//#include "PxPhysicsAPI.h"
 // TODO: This is an example of a library function
 void fnPhysicModule()
 {
