c9d68d5f3d9811e478adafc5d271bc546ed51bb1
Cynthia Tristán <cyntrist@ucm.es>
2026-02-27 00:56:56 +0100

    Create Transform

---
+++ src/Core/Transform.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Component.h"
+#include "Vector3.h"
+
+class Transform : public ec::Component
+{
+	Vector3 position;
+	//Quaternion rotation;
+	Vector3	scale;
+public:
+	//Transform();
+	//~Transform();
+};
