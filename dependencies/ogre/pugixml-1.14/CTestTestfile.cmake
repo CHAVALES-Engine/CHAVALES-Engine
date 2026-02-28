4123cb60713366f06ebf1bbdb98886c6b6bdfa3e
MANK1NG <98019598+MANK1NG@users.noreply.github.com>
2026-02-28 20:54:10 +0100

    limpieza en la carpeta de ogre

--- dependencies/ogre/pugixml-1.14/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ivani/OneDrive/Escritorio/build/pugixml-1.14
-# Build directory: C:/Users/ivani/OneDrive/Escritorio/build/pugixml-1.14
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
