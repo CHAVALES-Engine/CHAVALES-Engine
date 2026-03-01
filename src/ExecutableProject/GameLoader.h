ccc42c7bddeb47179291e50dedcc0ab3f989b629
nievesag <nievalon@ucm.es>
2026-03-01 19:40:55 +0100

    no usar pch en componentsproject

--- src/ExecutableProject/GameLoader.h
+++ src/ExecutableProject/GameLoader.h
@@ -1,5 +1,28 @@
 #pragma once
+
+#include "Scene.h"
+
 class GameLoader
 {
+public:
+	GameLoader() = default;
+	~GameLoader() = default;
+
+	void loadScene();
+
+
+
+	/*
+	Entity e
+	e.addComponent(c)
+
+	Scene s
+	s.addEntity(e)
+
+	StateMachine
+	sm.addScene(s)
+	*/
+
+private:
 
 };
