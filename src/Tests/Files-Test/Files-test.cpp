0c558c0f049ad1226272d2fde8023c9bb0774d1a
Cynthia Tristán <cyntrist@ucm.es>
2026-03-02 01:13:58 +0100

    progreso de configuracion files-test?

---
+++ src/Tests/Files-Test/Files-test.cpp
@@ -0,0 +1,42 @@
+// Files-test.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+namespace fs = std::filesystem;
+#define SOL_ALL_SAFETIES_ON 1
+#include <sol/sol.hpp>
+
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+  //  std::string path = "./hola";
+  //  std::string file;
+  //  for (const auto& entry : fs::directory_iterator(path))
+  //  {
+		//file = entry.path().generic_string();
+
+		//std::ifstream entrada(file);
+
+		//if (!entrada.is_open())
+		//{
+		//	return -1;
+		//}
+
+		//auto cinbuf = std::cin.rdbuf(entrada.rdbuf());
+
+		//std::string entidadName;
+		//std::cin >> entidadName;
+
+		//std::cout << entidadName << '\n';
+
+		//std::cin.rdbuf(cinbuf);
+  //  }
+
+	sol::state lua;
+	lua.script_file("./scenes/scene_prueba.lua");
+
+
+}
