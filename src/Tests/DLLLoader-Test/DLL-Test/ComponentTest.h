1911b8ef5757ae4be5b827c8b81215bb6f704efb
Cynthia Tristán <cyntrist@ucm.es>
2026-03-02 20:13:20 +0100

    filetest linked error

--- src/Tests/DLLLoader-Test/DLL-Test/ComponentTest.h
+++ src/Tests/DLLLoader-Test/DLL-Test/ComponentTest.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <Component.h>
 #include <PluginSDK.h>
+#include "Debug.h"
+
 class ComponentTest : public core::Component
 {
-
+	void fixedUpdate() override
+	{
+		core::Debug::out(core::Debug::DebugMode::DEBUG_BOTH, "Esto es el FixedUpdate de un componente de tipo ComponentTest y mi entidad se llama ", getEntity(), "\n");
+	}
 };
 
 REGISTER_COMPONENT(ComponentTest);
--- src/Tests/Files-Test/Files-test.cpp
+++ src/Tests/Files-Test/Files-test.cpp
@@ -1,41 +1,22 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+
+#include "Debug.h"
 namespace fs = std::filesystem;
 #define SOL_ALL_SAFETIES_ON 1
 #include <sol/sol.hpp>
 
 #include <fstream>
 #include "Entity.h"
+#include "Component.h"
 #include "Scene.h"
-
 #include "../../ExecutableProject/GameLoader.h"
+#include "ComponentRegister.h"
+#include "../../ExecutableProject/DLLLoader.h"
 
 int main(int argc, char* argv[])
 {
-  //  std::string path = "./hola";
-  //  std::string file;
-  //  for (const auto& entry : fs::directory_iterator(path))
-  //  {
-		//file = entry.path().generic_string();
-
-		//std::ifstream entrada(file);
-
-		//if (!entrada.is_open())
-		//{
-		//	return -1;
-		//}
-
-		//auto cinbuf = std::cin.rdbuf(entrada.rdbuf());
-
-		//std::string entidadName;
-		//std::cin >> entidadName;
-
-		//std::cout << entidadName << '\n';
-
-		//std::cin.rdbuf(cinbuf);
-  //  }
-
 	sol::state lua;
 	lua.script_file("./scenes/scene_prueba.lua");
 	sol::table scene = lua["scene"];
@@ -59,21 +40,17 @@ int main(int argc, char* argv[])
 			auto componenteObj = componente.first;
 			std::string componenteName = componenteObj.as<std::string>();
 			std::cout << "COMPONENTE: " << componenteName << std::endl;
+			std::unique_ptr<core::Component> component = ComponentRegister::instance().create(componenteName);
 
-			if (componenteName == "Transform")
+			if (component != nullptr)
 			{
-				// busca en el mapa de componentes y lo construye
-
 				// mete el componente a la entidad creada
-				//e->addComponent<>();
-			}
-			else if (componenteName == "ComponentTest")
-			{
+				//e->addComponent(component);
 				
 			}
 			else
 			{
-				std::cout << "Componente " << componenteName << " no registrado" << '\n';
+				core::Debug::warning("Componente ", componenteName, " no registrado\n");
 			}
 		}
 
@@ -84,3 +61,26 @@ int main(int argc, char* argv[])
 	s->update(10);
 	delete s;
 }
+
+//  std::string path = "./hola";
+//  std::string file;
+//  for (const auto& entry : fs::directory_iterator(path))
+//  {
+	  //file = entry.path().generic_string();
+
+	  //std::ifstream entrada(file);
+
+	  //if (!entrada.is_open())
+	  //{
+	  //	return -1;
+	  //}
+
+	  //auto cinbuf = std::cin.rdbuf(entrada.rdbuf());
+
+	  //std::string entidadName;
+	  //std::cin >> entidadName;
+
+	  //std::cout << entidadName << '\n';
+
+	  //std::cin.rdbuf(cinbuf);
+//  }
