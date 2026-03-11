46ef5895744dbdfaab9d24ab7fe9ce4ccf311b2f
Andrés García Navarro <andrga37@ucm.es>
2026-03-11 20:52:01 +0100

    input skeleton on platform

--- docs/update_docs.bat
+++ docs/update_docs.bat
@@ -1,5 +1,2 @@
 @echo off
-echo ================================
-echo LIMPIANDO CARPETAS ANTIGUAS
-echo ================================
 doxygen Doxyfile
--- src/PlatformModule/PlatformModule.h
+++ src/PlatformModule/PlatformModule.h
@@ -7,27 +7,19 @@
 
 #include <string>
 #include <unordered_map>
-#include <variant>
 #include <Windows.h>
+#include "InputDefs.h"
 
-struct SDL_Window; 
+struct SDL_Window;
 union SDL_Event;
-
-class VirtualDevice;
-enum Key;
-enum MouseButton;
-enum MouseAxis;
-enum GamepadButton;
-enum GamepadAxis;
-using InputAction = std::variant<
-	Key,
-	MouseButton,
-	MouseAxis,
-	GamepadButton,
-	GamepadAxis>;
+namespace input
+{
+	class InputMapper;
+	class VirtualDevice;
+}
 
 /**
-* @brief Modulo de Plataforma
+* @brief Modulo de Plataforma, encargado de gestionar la ventana y los eventos de input.
 */
 class PlatformModule
 {
@@ -52,41 +44,69 @@ public:
 	bool Init();
 	/**
 	* @brief Devuelve el handle de la ventana de Windows
-	*		
+	*
 	* @return HWND - Hanlde de la ventana (internamente un id)
 	*/
-	const HWND getWindowHandle() const;
+	HWND getWindowHandle() const;
 	/**
 	* @brief Actualiza eventos
 	*
 	* @return bool - Booleano para saber si se ha cerrado la ventana
 	*/
-	const bool syncronize();
+	bool syncronize();
 	/**
 	* @brief Devuelve anchura de la ventana
 	*/
-	const int getWindowWidth() const;
+	int getWindowWidth() const;
 	/**
 	* @brief Devuelve altura de la ventana
 	*/
-	const int getWindowHeight() const;
-
-	const bool isKeyPressed() const;
-
-	const bool isKeyReleased() const;
-
-	const float getAxis() const;
-
-	const bool isActionPressed(std::string) const;
-
-	const bool isActionReleased(std::string) const;
-
+	int getWindowHeight() const;
+	/*
+	 * @brief Devuelve si una tecla esta pulsada
+	 * @param inputAction - InputAction a comprobar
+	 * @param device - id del dispositivo a comprobar. -1 por defecto => el primero positivo que encuentre.
+	 */
+	bool isKeyPressed(input::InputAction inputAction, input::DeviceID device = -1) const;
+	/*
+	 * @brief Devuelve si se ha dejado de pulsar una tecla
+	 * @param inputAction - InputAction a comprobar
+	 * @param device - id del dispositivo a comprobar. -1 por defecto => el primero positivo que encuentre.
+	 */
+	bool isKeyReleased(input::InputAction inputAction, input::DeviceID device = -1) const;
+	/*
+	 * @brief Devuelve cuanto de accionado esta la accion a comprobar
+	 * @param inputAction - InputAction a comprobar
+	 * @param device - id del dispositivo a comprobar. -1 por defecto => el primero positivo que encuentre.
+	 * @return float - Devuelve de -1 a 1
+	 */
+	float getAxis(input::InputAction inputAction, input::DeviceID device = -1) const;
+	/*
+	 * @brief Devuelve si se ha pulsado una accion
+	 * @param actionName - accion a comprobar
+	 * @param device - id del dispositivo a comprobar. -1 por defecto => el primero positivo que encuentre.
+	 */
+	bool isActionPressed(std::string actionName, input::DeviceID device = -1) const;
+	/*
+	 * @brief Devuelve si se ha dejado de pulsar una accion
+	 * @param actionName - accion a comprobar
+	 * @param device - id del dispositivo a comprobar. -1 por defecto => el primero positivo que encuentre.
+	 */
+	bool isActionReleased(std::string actionName, input::DeviceID device = -1) const;
+	/*
+	 * @brief Getter del input mapper para registrar acciones
+	 * @return input::InputMapper& - referencia al InputMapper
+	 */
+	input::InputMapper& getInputMapper();
 private:
 	/**
 	* @brief procesa un evento de sdl
 	*/
 	const void processEvent(const SDL_Event& event);
-	std::unordered_map<uint8_t, VirtualDevice> _virtualDevices;
+	/**
+	* @brief ´mapa de dispositivos virtuales
+	*/
+	std::unordered_map<uint8_t, input::VirtualDevice> _virtualDevices;
 	/**
 	* @brief Ventana de SDL
 	*/
