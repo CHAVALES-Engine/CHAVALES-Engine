1ac7cdf96f82a8138eb3089fc04b5d34863d68a8
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-03-24 09:29:59 +0100

    rename

--- src/Engine/StateMachine.cpp
+++ src/Engine/StateMachine.cpp
@@ -44,7 +44,7 @@ void StateMachine::gameLoop()
 
 			if (GameLoader::reloadLua()) // si es necesario recargar...
 			{
-				_currentScene.ptr->endGame(); // elimina escena anterior
+				_currentScene.ptr->clearScene(); // elimina escena anterior
 				scenePtr s = std::move(GameLoader::loadScene(_currentScene.name)); // vuelve a cargar
 				_currentScene.ptr = s;
 			}
