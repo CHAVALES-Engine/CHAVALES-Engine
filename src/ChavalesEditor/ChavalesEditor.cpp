9d19ef182b266bc5a287be9b98db0ab996f7e35c
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-03-24 10:59:15 +0100

    imgui progreso

--- src/ChavalesEditor/ChavalesEditor.cpp
+++ src/ChavalesEditor/ChavalesEditor.cpp
@@ -1,11 +1,22 @@
-// ChavalesEditor.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <functional>
 #include <iostream>
 #include <vector>
 
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Setup Dear ImGui context
+    //IMGUI_CHECKVERSION();
+    //ImGui::CreateContext();
+    //ImGuiIO& io = ImGui::GetIO();
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+    // Setup Platform/Renderer backends
+    //ImGui_ImplSDL2_InitForOpenGL(window, YOUR_SDL_GL_CONTEXT);
+    //ImGui_ImplOpenGL3_Init();
 }
