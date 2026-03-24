018c249640dffc9824a071ee8d5f4cdcfd9a2dae
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-03-24 10:36:30 +0100

    dear imgui master

---
+++ src/ChavalesEditor/ChavalesEditor.cpp
@@ -0,0 +1,11 @@
+// ChavalesEditor.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <functional>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
