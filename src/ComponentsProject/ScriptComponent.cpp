210216e54cff847ffaebfbc4c438d1e62f1cad88
Andrés García Navarro <andrga37@ucm.es>
2026-04-15 23:38:24 +0200

    script component

---
+++ src/ComponentsProject/ScriptComponent.cpp
@@ -0,0 +1,46 @@
+﻿#include "ScriptComponent.h"
+//#include "Script.h"
+#include "checkMLNew.h"
+
+ScriptComponent::ScriptComponent()//: _script(std::make_unique<Script>())
+{}
+
+bool ScriptComponent::init(const Properties& p)
+{
+	std::string scriptPath;
+	/*if (!setProperty(p, "script", scriptPath) ||
+		!_script->loadScript(scriptPath))
+		return false;*/
+	// inicializacion de propiedades TODO
+	return true;
+}
+
+void ScriptComponent::ready()
+{
+	//_script->executeFunction("ready");
+}
+
+void ScriptComponent::enable()
+{
+	//_script->executeFunction("enable");
+}
+
+void ScriptComponent::fixedUpdate()
+{
+	//_script->executeFunction("fixedUpdate");
+}
+
+void ScriptComponent::update(uint64_t deltaTime)
+{
+	//_script->executeFunction("update", deltaTime);
+}
+
+void ScriptComponent::disable()
+{
+	//_script->executeFunction("disable");
+}
+
+void ScriptComponent::destroy()
+{
+	//_script->executeFunction("destroy");
+}
---
+++ src/ComponentsProject/ScriptComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Component.h"
+
+class Script;
+
+class ScriptComponent :
+    public  core::Component
+{
+public:
+    ScriptComponent();
+    ~ScriptComponent() = default;
+
+    bool init(const Properties& p) override;
+    void ready()                   override;
+    void enable()                  override;
+    void fixedUpdate()             override;
+    void update(uint64_t deltaTime)override;
+    void disable()                 override;
+    void destroy()                 override;
+private:
+    std::unique_ptr<Script> _script;
+};
+
