b86a2a714f9ecde36883df5c6dc6be3170ed6b38
Ismaorte <ismaorte@ucm.es>
2026-04-18 13:24:43 +0200

    Comentarios

--- src/ResourcesModule/ResourcesModule.h
+++ src/ResourcesModule/ResourcesModule.h
@@ -7,26 +7,6 @@
 #include <sol.hpp>
 #include "guid.h"
 
-/*
- * +------------------+
- * | RESOURCES MODULE |
- * +------------------+
- *
- * --- Ejemplo de uso en lua ---
- * assets = {
- *		Mesh = {
- *			metroid = {
- *				sourceFolder = "metroid-floating/source", -> esta es la ruta donde se encuntra el modelo (para particulas y texturas igual)
- *				fileName = "metroid_final.fbx" -> esta es la malla (para particulas y texturas igual)
- *			}
- *		},
- *		Audio = {
- *			lifeUp = "C:/2526-Grupo03-ChavalesEngine/bin/game/scenes/assets/smb_1-up.wav" -> nombre del audio = ruta del audio
- *		}
- * }
-*/
-
-
 class ResourcesModule
 {
 public:
@@ -35,18 +15,38 @@ public:
 
 	bool Init();
 
+	/// <summary>
+	/// Method to retrive the path of the desire asset
+	/// </summary>
+	/// <param name="assetName">Name of the asset, this name is the real name of the file + the type of extension of the file</param>
+	/// <returns></returns>
 	std::string getAssetSourceFolder(std::string assetName);
+
+	/// <summary>
+	/// Method to the render module to retrive all fonts
+	/// </summary>
+	/// <returns></returns>
 	std::vector<std::pair<std::string, std::string>> getAllFonts();
 
 private:
-	bool loadAsset(std::string sourceName);
+	/// <summary>
+	/// Method to go overall the assets folders, it's recursive which means it will travel across the folder unitl it has reserved all the assets of that folder
+	/// </summary>
+	/// <param name="sourceName"> Name of the folder where the assets are</param>
+	/// <returns></returns>
+	bool loadAsset(std::string sourceName); 
 
-	bool insertAssetMap(std::string sourceName);
+	/// <summary>
+	/// Method to insert into the maps
+	/// </summary>
+	/// <param name="sourceName">The path of file in the folder of assets</param>
+	/// <returns></returns>
+	bool insertAssetMap(std::string sourceName); 
 
-	std::unordered_map<ChavalesGUID, std::string> _idMaps;
-	std::unordered_map<std::string, ChavalesGUID> _assetsMaps;
+	std::unordered_map<ChavalesGUID, std::string> _idMaps; //Map of IDs with the path of the associated asset
+	std::unordered_map<std::string, ChavalesGUID> _assetsMaps; //Map of assets sorted by name and with an ID
 
-	std::string typeOfFolder;
-	std::vector<std::pair<std::string, std::string>> _fontsVector;
+	std::string typeOfFolder; //String of the folder to identify the type of assset and in which the folder is it
+	std::vector<std::pair<std::string, std::string>> _fontsVector; //Vector to save all fonts, in order to render load them
 };
 
