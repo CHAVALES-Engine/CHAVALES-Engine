dc3fb047d729fcd075361a8c8560d76c7d1dd615
Cynthia Tristán <cyntrist@ucm.es>
2026-04-26 03:08:21 +0200

    Update ComponentTest.h

--- src/Tests/DLLLoader-Test/DLL-Test/ComponentTest.h
+++ src/Tests/DLLLoader-Test/DLL-Test/ComponentTest.h
@@ -198,7 +198,7 @@ public:
 	{
 		Debug::out("CAMBIO DE ESCENA");
 
-		Engine::instance()->addAndSetScene("scene2");
+		Engine::instance()->requestSceneChange("scene2");
 	}
 
 	void fixedUpdate() override
