f76223acb51f8855d74597b37deb982776206d42
Andrés García Navarro <andrga37@ucm.es>
2026-05-03 15:51:04 +0200

    Update Message.h

--- src/Core-Defs/Message.h
+++ src/Core-Defs/Message.h
@@ -31,7 +31,7 @@ namespace core
 		{
 			for (int i = 0; i < _subscribers.size(); i++)
 			{
-				Debug::out("EJECUTANDO SUBSCRIBER");
+				//Debug::out("EJECUTANDO SUBSCRIBER");
 				_subscribers[i](args...);
 			}
 		}
