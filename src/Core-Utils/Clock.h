181c4613ae2d4b4c4d502824954216a3dacaa2e4
Cynthia Tristán <cyntrist@ucm.es>
2026-05-20 23:06:09 +0200

    doxyconfig
    Update Doxyfile

--- src/Core-Utils/Clock.h
+++ src/Core-Utils/Clock.h
@@ -19,14 +19,12 @@ namespace core
 		Clock() = default;
 		~Clock() = default;
 
-		/** @brief
-		*	Tiempo desde la ultima actualizacion.
+		/** @brief Tiempo desde la ultima actualizacion.
 		*/
 		static inline uint64_t deltaTime = 0;
 
 		/**
-		* @brief
-		*	Tasa de frames por milisegundo. 60 FPS.
+		* @brief Tasa de frames por milisegundo. 60 FPS.
 		*/
 		static constexpr uint64_t FRAME_RATE = 1000 / 60; // 1000 ms / 60 frames
 
@@ -36,35 +34,34 @@ namespace core
 		static constexpr int MAX_FIXED_STEPS = 4;
 
 
-		/** @brief
-		*	Actualiza deltaTime.
-		*	
+		/** @brief Actualiza deltaTime.
 		*	@param dt - deltaTime.
 		*/
 		static inline void setDeltaTime(uint64_t dt);
 
-		/** @brief
-		*	Calcula el valor de deltaTime desde un tiempo inicial dado.
+		/** @brief Calcula el valor de deltaTime desde un tiempo inicial dado.
 		*	@param sT - Tiempo desde el que calcular el deltaTime.
 		*/
 		static inline uint64_t calculateDeltaTime(std::chrono::high_resolution_clock::time_point sT);
+
+		/** @brief Calcula el valor de deltaTime desde un tiempo inicial dado.
+		*	@param start - momento inicial en ms.
+		*	@param end - momento final en ms.
+		*/
 		static inline uint64_t calculateDeltaTime(
 			std::chrono::high_resolution_clock::time_point start,
 			std::chrono::high_resolution_clock::time_point end);
 
-		/** @brief
-		*	Tiempo actual.
+		/** @brief Tiempo actual.
 		*/
 		static inline std::chrono::high_resolution_clock::time_point getNow();
 
-		/** @brief
-		*	Tiempo que lleva el programa en ejecucion.
+		/** @brief Tiempo que lleva el programa en ejecucion.
 		*/
 		static inline uint64_t getRunningTime();
 
 	private:
-		/** @brief
-		*	Tiempo en el que comienza el programa.
+		/** @brief Tiempo en el que comienza el programa.
 		*/
 		static std::chrono::high_resolution_clock::time_point _startTime;
 	};
